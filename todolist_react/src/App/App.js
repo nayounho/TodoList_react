ac659f75c69d21cb9d339dddddf18f3d8d1a9395
younho <nnyyhh9247@gmail.com>
2021-04-02 12:19:29 +0900

    todoList-setup

---
+++ todolist_react/src/App/App.js
@@ -0,0 +1,13 @@
+import HeadSpan from '../Conponent/HeadSpan/HeadSpan';
+import Input from '../Conponent/Input/Input';
+
+function App() {
+  return (
+    <>
+      <HeadSpan />
+      <Input />
+    </>
+  );
+}
+
+export default App;
---
+++ todolist_react/src/Conponent/HeadSpan/HeadSpan.js
@@ -0,0 +1,5 @@
+const HeadSpan = () => {
+  return <h1>Todo-list</h1>;
+};
+
+export default HeadSpan;
